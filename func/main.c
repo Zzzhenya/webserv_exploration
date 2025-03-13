fe31390165a094a3750b92aa3ceb7fe0dd775e70
Shenya De Silva <shenya44@outlook.com>
2025-03-14 00:57:50 +0100

    Test: 3

---
+++ func/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+	printf("ERROR\n");
+	return (0);
+}
--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 ls
 ls -la
 echo "haha"
-./abc
+cd func && cc main.c
+if ./a.out | grep "ERROR"; then
+	printf " Error\n"
+else
+	printf "\tAll OK!\n"
+fi
